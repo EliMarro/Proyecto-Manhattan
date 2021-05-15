7fb529127a12556b5a8c4318518a41325e40be75
diana-stg <80397861+diana-stg@users.noreply.github.com>
2021-05-15 18:57:06 +0200

    Add files via upload

--- Prep_con una imagen.py
+++ Prep_con una imagen.py
@@ -9,13 +9,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+from sklearn.model_selection import train_test_split
+
+
+
 filePath= "Metadatos.xlsx"
 meta = load_workbook(filePath)
 sheet = meta.active
 x = sheet.max_row
 
 a=[] #lista para hacer luego numpy concatenate
-for i in range(2,5):
+for i in range(2,12):
     #contenido = os.listdir(sheet.cell(row=i, column= 17).value)
 
     with os.scandir("D:\\Asignaturas\\Phyton y JavaScript\\imágenes cancer\\manifest-1616439774456" + sheet.cell(row=i, column= 17).value) as ficheros: # me voy a la carpeta que me marca el excel y escaneo los elementos que hay en la carpeta y los llamo "ficheros"
@@ -47,8 +51,23 @@ y2 = y.max()
 z1 = z.min()
 z2 = z.max()
 algo1= algo[z1:z2+1, x1:x2, y1:y2] #recortas el tensor 
-print(algo1.shape)          
+print(algo1.shape)      
 
+
+X_train, X_test = train_test_split(algo1, test_size=0.2) #para dividir las imágenes en 80% train y 20% test
+print(X_train.shape)
+print(X_test.shape)
+
+
+np.save('X_train',X_train)
+train = np.load('X_train.npy')
+print(X_train.shape)
+#para comprobar que lo guarda bien
+
+np.save('X_test',X_test)
+test = np.load('X_test.npy')
+print(X_test.shape)
+#para comprobar que lo guarda bien
 
             
             
